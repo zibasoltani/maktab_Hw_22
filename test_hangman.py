e12ae1611a5982d4d0c3a7c3636c39b09c066aa9
ZEYNAB <zibasoltani73@gmail.com>
2024-05-30 02:11:01 +0330

    test_hangman.py

--- test_hangman.py
+++ test_hangman.py
@@ -1,1 +1,17 @@
+import unittest
+from hangman import get_random_word, hangmangame
 
+
+class TestHangman(unittest.TestCase):
+    def setUp(self):
+        self.word = get_random_word()
+        self.game = hangmangame()
+
+    def test_hangman_revealed_letter_matches(self):
+        # self.game.guess()
+        updated_hangman_string = self.game.status()
+        self.assertEqual(updated_hangman_string, "_ _ _")
+
+
+if __name__ == '__main__':
+    unittest.main()
